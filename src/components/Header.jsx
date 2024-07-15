de271ede23f1cc0d88bcac64a8c711b748f3af2a
Woosterwebdev <136399860+Woosterwebdev@users.noreply.github.com>
2024-07-15 12:04:59 -0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react"
+import trollface from "../assets/troll-face.png"
 
 export default function Header() {
     return (
         <header className="header">
             <img 
-                src="./src/assets/troll-face.png" 
+                src={trollface} 
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
